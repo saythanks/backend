931d5430a4dd1289a4c2fba21a95f12bf58bd2c1
Oscar <newman.oscar@gmail.com>
2019-02-04 14:15:40 -0500

    added stripe integratoin

--- backend/payments/balance.py
+++ backend/payments/balance.py
@@ -1,8 +1,32 @@
 import functools
 from flask import request, render_template, jsonify
+from flask_cors import cross_origin
+import stripe
 from backend.payments import bp
+
 
 # Handles route that retrieves a user's balance
 @bp.route('/balance', methods=['GET'])
-def balance():
-    return jsonify({'balance': 13.25, 'monthly_spend': 1.40})
+def index():
+
+    return jsonify({'balance': 13.50, 'monthly_spend': 1.50})
+
+# Handles route that retrieves a user's balance
+@bp.route('/balance', methods=['POST'])
+@cross_origin()
+def create():
+	stripe.api_key = "sk_test_ssrVf0FK8RDjJVrPozoWT1iq"
+	json = request.get_json()
+	token = json['token']
+	amount = json['amount']
+
+
+	charge = stripe.Charge.create(
+		amount=amount,
+		currency='usd',
+		description='Add to Account',
+		source=token
+	)
+
+
+	return jsonify({ 'charge': 3 })

8d8eba10d7befa6420a158a7c23e5bad12733288
Oscar <newman.oscar@gmail.com>
2019-02-11 10:21:10 -0500

    whoops

--- backend/util/firebase.py
+++ backend/util/firebase.py
@@ -3,7 +3,5 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate(
-    os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
 cred = credentials.Certificate("keys/firebase.json")
 default_app = firebase_admin.initialize_app(cred)
